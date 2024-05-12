84ad66bc0c62b8c0281375c071f0c1f31e4ded15
zip8919 <110547378+zip8919@users.noreply.github.com>
2024-05-12 16:25:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 npm install vuepress-theme-reco --save-dev
+yarn add vuepress-theme-reco
 npm install vuepress-plugin-social-share -D
 npm install vuepress-plugin-code-copy
 npm i vuepress-plugin-reading-progress
