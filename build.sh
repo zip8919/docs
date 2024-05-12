01a9ab963228055626f78d3b48b402c145e59fde
zip8919 <110547378+zip8919@users.noreply.github.com>
2024-05-12 15:21:36 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+npm i -D @vuepress/plugin-back-to-top@next
+npm i -D @vuepress/plugin-copy-code@next
+npm i -D vuepress-plugin-md-enhance
+npm install vuepress-plugin-social-share@next -D
+npm i -D @vuepress/plugin-reading-time@next
+npm i -D @vuepress/plugin-catalog@next
+npm i
+npm run docs:build
