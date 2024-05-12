1c9638afe768ce8f13acff1d23165ff5c1f5df8d
zip8919 <110547378+zip8919@users.noreply.github.com>
2024-05-12 16:46:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-npm i @ikangxu/vuepress-plugin-share -D
-npm install vuepress-plugin-social-share -D
 npm install vuepress-plugin-code-copy
 npm i vuepress-plugin-reading-progress
 npm install -D vuepress-plugin-reading-time
